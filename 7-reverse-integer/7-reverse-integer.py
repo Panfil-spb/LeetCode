c825738a1b23a90fd522d4880376c0e9b24c609b
Andrey <64960247+Panfil-spb@users.noreply.github.com>
2022-06-21 18:28:08 +0300

    Time: 56 ms (31.41%), Space: 14 MB (15.67%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -1,9 +1,10 @@
 class Solution:
     def reverse(self, x: int) -> int:
         res = 0
-        xs = str(abs(x))
-        for i in range(len(xs)):
-            res += int(xs[i]) * (10 ** i)
+        xs = abs(x)
+        while xs > 0:
+            res = res * 10 + xs % 10
+            xs //= 10
         
         if x >= 0:
             if res > (2 ** 31) - 1:
