8625c83d3d839ba9f4f41ead6560280ebdbe23eb
Andrey <64960247+Panfil-spb@users.noreply.github.com>
2022-06-21 19:20:33 +0300

    Time: 494 ms (31.51%), Space: 0B (100.00%) - LeetHub

--- 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
@@ -1,4 +1,4 @@
-/* Write your T-SQL query statement below */
+# Write your MySQL query statement below
 select a.name as Employee
 from Employee as a, Employee as b
 where a.ManagerId = b.id and a.salary > b.salary
